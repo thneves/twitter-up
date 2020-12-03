6b36942223f542053fe45f0040b09208b6633832
thneves <tsneves11@gmail.com>
2020-12-03 16:23:17 -0300

    create profile views

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -16,17 +16,18 @@ class TweeetsController < ApplicationController
 
   # GET /tweeets/new
   def new
-    @tweeet = Tweeet.new
+    @tweeet = current_user.tweeets.build
   end
 
   # GET /tweeets/1/edit
   def edit
+    
   end
 
   # POST /tweeets
   # POST /tweeets.json
   def create
-    @tweeet = Tweeet.new(tweeet_params)
+    @tweeet = current_user.tweeets.build(tweeet_params)
 
     respond_to do |format|
       if @tweeet.save
